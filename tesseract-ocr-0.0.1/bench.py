4499f49b39617e6cca2ac4b5fd9b57a155fcc3c8
Ananda Subramanian <sssanand70@gmail.com>
2023-12-03 21:47:17 +0530

    OCR 2.0.1 (UI - In process)

--- tesseract-ocr-0.0.1/bench.py
+++ tesseract-ocr-0.0.1/bench.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import time
-import pytesseract
+
+from cassandra.concurrent import xrange
+
 import tesseract_ocr
 
 start = time.time()
 for i in xrange(100):
     tesseract_ocr.text_for_filename('tests/code.tiff')
-print time.time() - start
+print(time.time() - start)
